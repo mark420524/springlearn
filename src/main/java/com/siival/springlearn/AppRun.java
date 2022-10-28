e4664195f3d08b1c33c7eedc9ab3254f0d501987
mark <markjava@foxmail.com>
2022-10-28 13:15:13 +0800

    init project springlearn

---
+++ src/main/java/com/siival/springlearn/AppRun.java
@@ -0,0 +1,13 @@
+package com.siival.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRun {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AppRun.class, args);
+	}
+
+}
